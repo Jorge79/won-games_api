74b6ef1ab1f703dfa83e5f926e11435a4a10fb1e
Jorge Francisco <Jorge7.9@hotmail.com>
2022-04-12 13:09:38 -0300

    feat: category collection type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b05c07493cc77a76928c183588509ad1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '26257de5-34fa-4f74-91bf-ad343d74edfd'
+};
