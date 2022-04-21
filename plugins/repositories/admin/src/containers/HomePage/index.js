2f80932565be05c6d585f7b3709eabd7807ed677
Jorge Francisco <Jorge7.9@hotmail.com>
2022-04-21 19:14:45 -0300

    feat: create the plugin page

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,67 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from "react";
+import axios from "axios";
+import { Table } from "@buffetjs/core";
+import { Header } from "@buffetjs/custom";
+import styled from "styled-components";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
+
+const headers = [
+  {
+    name: "Name",
+    value: "name",
+  },
+  {
+    name: "Description",
+    value: "description",
+  },
+  {
+    name: "Url",
+    value: "html_url",
+  },
+];
 
 const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/react-avancado/repos")
+      .then((res) => setRows(res.data))
+      .catch((e) =>
+        strapi.notification.error(`Ops... Github API error, ${e}.`)
+      );
+  }, []);
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: "React Avançado Repositories" }}
+        content="A list of our repositories in React Avançado course."
+      ></Header>
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
