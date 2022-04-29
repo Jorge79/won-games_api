e221264c475c32e1ce6342b471bc2816d630b5c6
Jorge Francisco <Jorge7.9@hotmail.com>
2022-04-29 00:56:23 -0300

    feat: create game structure

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,6 +23,40 @@ async function getGameInfo(slug) {
   };
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 async function getByName(name, entityName) {
   const item = await strapi.services[entityName].find({ name });
   return item.length ? item[0] : null;
@@ -79,6 +113,7 @@ module.exports = {
     // console.log(products[1]);
 
     await createManyToManyData([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
 
     // await create(products[1].publisher, "publisher");
     // await create(products[1].developer, "developer");
